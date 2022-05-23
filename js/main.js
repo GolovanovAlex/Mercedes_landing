b1842b9027673ec6edaa33801621987763e31b41
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-05-23 19:51:45 +0300

    add slider and arrow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(function () {
+  $('.design-slider').slick({
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img class="arrow arrow-prev" src="images/icon/arrow-prev.svg" alt="arrow-prev"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img class="arrow arrow-next" src="images/icon/arrow-next.svg" alt="arrow-next"></button>',
+  });
 });
