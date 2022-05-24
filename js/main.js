dca733759c6c1b514ec0325730e073c67635fcda
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-05-24 21:43:52 +0300

    @media slider

--- js/main.js
+++ js/main.js
@@ -6,5 +6,23 @@ $(function () {
       '<button type="button" class="slick-prev"><img class="arrow arrow-prev" src="images/icon/arrow-prev.svg" alt="arrow-prev"></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img class="arrow arrow-next" src="images/icon/arrow-next.svg" alt="arrow-next"></button>',
+    responsive: [
+      {
+        breakpoint: 1001,
+        settings: {
+          slidesToShow: 2,
+          variableWidth: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 656,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: false,
+          dots: true,
+        },
+      },
+    ],
   });
 });
