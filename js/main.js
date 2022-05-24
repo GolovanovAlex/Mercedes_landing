c28e9a91735e8c25c3cd9b53dbf8b00cfd1a0003
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-05-24 23:24:36 +0300

    scroll

--- js/main.js
+++ js/main.js
@@ -29,4 +29,10 @@ $(function () {
     $('.menu__list').toggleClass('active');
     $('.menu-burger').toggleClass('active');
   });
+  $('.menu').on('click', 'a', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1200);
+  });
 });
