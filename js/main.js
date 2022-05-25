e7d688819d127b161856f0f6dd5399afa73cf269
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-05-25 14:27:17 +0300

    add scrollUpTop

--- js/main.js
+++ js/main.js
@@ -36,3 +36,35 @@ $(function () {
     $('body,html').animate({ scrollTop: top }, 1200);
   });
 });
+
+// ---- scrollUpTop ---- //
+
+let offset = 500;
+let scrollUp = document.querySelector('.scroll-up');
+let scrollUpSvgPath = document.querySelector('.scroll-up__svg-path');
+let pathLength = scrollUpSvgPath.getTotalLength();
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+let getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+let updateDashoffset = () => {
+  let height = document.documentElement.scrollHeight - window.innerHeight;
+  let dashoffset = pathLength - (getTop() * pathLength) / height;
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+window.addEventListener('scroll', () => {
+  updateDashoffset();
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-up--active');
+  } else {
+    scrollUp.classList.remove('scroll-up--active');
+  }
+});
